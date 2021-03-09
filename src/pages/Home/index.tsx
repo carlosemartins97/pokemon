e88330eebebc48a9469cecba55b1b94b0f023b2a
Carlos Martins <carlosemartins97@gmail.com>
2021-03-09 19:03:34 -0300

    feat: create home header content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,11 @@ const Home: React.FC = () => {
             <AsideNav />
             <div className="wrapper-content">
                 <div className="content">
-                    <h1>teste</h1>
+                    <header className="content-header">
+                        <h1>Bem vindo(a) ao <span className="content-app-name">LOCALIZAMON</span></h1>
+                        <p>Aqui você poderá navegar pelos tipos de pokemons e conhecer mais sobre cada um deles.</p>
+                        <span className="content-header-span">Divirta-se!</span>
+                    </header>
                 </div>
             </div>
         </div>
